a95a77e1e1777b170c217970ff9f83571fe42e12
NexusXe <andastrike@gmail.com>
2023-10-19 14:02:23 +0000

    expand character set slightly

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@
 
 type StandardCharacter = u16; // 5 x 3, with sign bit as visibility
 type WideCharacter = u32; // 5 x 5, with sign bit as visibility and 6 trailing unused 0s
-type SmallCharacter = u8; // 3x2, with sign bit as visibility
+type SmallCharacter = u8; // 4x2
+
 
 type DisplayLine = u128;
 type DisplayPixels = [DisplayLine; 64];
@@ -23,44 +24,52 @@ const WIDECHAR_WIDTH: usize = 5;
 const SMALLCHAR_LENGTH: usize = 3;
 const SMALLCHAR_WIDTH: usize = 2;
 
-const A_PIXELS: StandardCharacter = 0b1111101111101101u16 as u16;
-const B_PIXELS: StandardCharacter = 0b1110101110101110u16 as u16;
-const C_PIXELS: StandardCharacter = 0b1111110110110111u16 as u16;
-const D_PIXELS: StandardCharacter = 0b1110101101101110u16 as u16;
-const E_PIXELS: StandardCharacter = 0b1111100111100111u16 as u16;
-const F_PIXELS: StandardCharacter = 0b1111100111100100u16 as u16;
-const G_PIXELS: StandardCharacter = 0b1111100101101111u16 as u16;
-const H_PIXELS: StandardCharacter = 0b1101101111101101u16 as u16;
-const I_PIXELS: StandardCharacter = 0b1111010010010111u16 as u16;
-const J_PIXELS: StandardCharacter = 0b1111010010010110u16 as u16;
-const K_PIXELS: StandardCharacter = 0b1101101110101101u16 as u16;
-const L_PIXELS: StandardCharacter = 0b1101101101101111u16 as u16;
-const M_PIXELS: WideCharacter = 0b11101110101101011000110001000000u32 as u32;
-const N_PIXELS: StandardCharacter = 0b1110101101101101u16 as u16;
-const O_PIXELS: StandardCharacter = 0b1111101101101111u16 as u16;
-const P_PIXELS: StandardCharacter = 0b1111101111100100u16 as u16;
-const Q_PIXELS: StandardCharacter = 0b1111101101111001u16 as u16;
-const R_PIXELS: StandardCharacter = 0b1111101110101101u16 as u16;
-const S_PIXELS: StandardCharacter = 0b1111100111001111u16 as u16;
-const T_PIXELS: StandardCharacter = 0b1111010010010010u16 as u16;
-const U_PIXELS: StandardCharacter = 0b1101101101101111u16 as u16;
-const V_PIXELS: StandardCharacter = 0b1101101101101010u16 as u16;
-const W_PIXELS: WideCharacter = 0b11000110001101011010111011000000u32 as u32;
-const X_PIXELS: StandardCharacter = 0b1101101010101101u16 as u16;
-const Y_PIXELS: StandardCharacter = 0b1101101101010010u16 as u16;
-const Z_PIXELS: StandardCharacter = 0b1111001010100111u16 as u16;
-const ZERO_PIXELS: StandardCharacter = 0b1111101101101111u16 as u16;
-const ONE_PIXELS: StandardCharacter = 0b1110010010010111u16 as u16;
-const TWO_PIXELS: StandardCharacter = 0b1111001011100111u16 as u16;
-const THREE_PIXELS: StandardCharacter = 0b1111001111001111u16 as u16;
-const FOUR_PIXELS: StandardCharacter = 0b1101101111001001u16 as u16;
-const FIVE_PIXELS: StandardCharacter = 0b1111100111001110u16 as u16;
-const SIX_PIXELS: StandardCharacter = 0b1111110111101111u16 as u16;
-const SEVEN_PIXELS: StandardCharacter = 0b1111001010010010u16 as u16;
-const EIGHT_PIXELS: StandardCharacter = 0b1111101111101111u16 as u16;
-const NINE_PIXELS: StandardCharacter = 0b1111101111001001u16 as u16;
-const SLASH_PIXELS: StandardCharacter = 0b0001001010100100u16 as u16;
-const UNKNOWN_PIXELS: StandardCharacter = 0b1101010101010101u16 as u16;
+const SPACE_PIXELS: StandardCharacter = 0b1000000000000000u16;
+const EXCL_PIXELS: StandardCharacter = 0b1010010010000010u16;
+const QUOTE_PIXELS: StandardCharacter = 0b1101101000000000u16;
+const HASH_PIXELS: StandardCharacter = 0b1101111101111101u16;
+const DOLLAR_PIXELS: WideCharacter = 0b10111010100011100010101110000000u32;
+// TODO: finish ASCII
+const A_PIXELS: StandardCharacter = 0b1111101111101101u16;
+const B_PIXELS: StandardCharacter = 0b1110101110101110u16;
+const C_PIXELS: StandardCharacter = 0b1111110110110111u16;
+const D_PIXELS: StandardCharacter = 0b1110101101101110u16;
+const E_PIXELS: StandardCharacter = 0b1111100111100111u16;
+const F_PIXELS: StandardCharacter = 0b1111100111100100u16;
+const G_PIXELS: StandardCharacter = 0b1111100101101111u16;
+const H_PIXELS: StandardCharacter = 0b1101101111101101u16;
+const I_PIXELS: StandardCharacter = 0b1111010010010111u16;
+const J_PIXELS: StandardCharacter = 0b1111010010010110u16;
+const K_PIXELS: StandardCharacter = 0b1101101110101101u16;
+const L_PIXELS: StandardCharacter = 0b1101101101101111u16;
+const M_PIXELS: WideCharacter = 0b11101110101101011000110001000000u32;
+const N_PIXELS: StandardCharacter = 0b1110101101101101u16;
+const O_PIXELS: StandardCharacter = 0b1111101101101111u16;
+const P_PIXELS: StandardCharacter = 0b1111101111100100u16;
+const Q_PIXELS: StandardCharacter = 0b1111101101111001u16;
+const R_PIXELS: StandardCharacter = 0b1111101110101101u16;
+const S_PIXELS: StandardCharacter = 0b1111100111001111u16;
+const T_PIXELS: StandardCharacter = 0b1111010010010010u16;
+const U_PIXELS: StandardCharacter = 0b1101101101101111u16;
+const V_PIXELS: StandardCharacter = 0b1101101101101010u16;
+const W_PIXELS: WideCharacter = 0b11000110001101011010111011000000u32;
+const X_PIXELS: StandardCharacter = 0b1101101010101101u16;
+const Y_PIXELS: StandardCharacter = 0b1101101101010010u16;
+const Z_PIXELS: StandardCharacter = 0b1111001010100111u16;
+const ZERO_PIXELS: StandardCharacter = 0b1111101101101111u16;
+const ONE_PIXELS: StandardCharacter = 0b1110010010010111u16;
+const TWO_PIXELS: StandardCharacter = 0b1111001011100111u16;
+const THREE_PIXELS: StandardCharacter = 0b1111001111001111u16;
+const FOUR_PIXELS: StandardCharacter = 0b1101101111001001u16;
+const FIVE_PIXELS: StandardCharacter = 0b1111100111001110u16;
+const SIX_PIXELS: StandardCharacter = 0b1111110111101111u16;
+const SEVEN_PIXELS: StandardCharacter = 0b1111001010010010u16;
+const EIGHT_PIXELS: StandardCharacter = 0b1111101111101111u16;
+const NINE_PIXELS: StandardCharacter = 0b1111101111001001u16;
+const SLASH_PIXELS: StandardCharacter = 0b0001001010100100u16;
+const DOT_PIXELS: SmallCharacter = 0b01100110u8;
+const COMMA_PIXELS: SmallCharacter = 0b01100010u8;
+const UNKNOWN_PIXELS: StandardCharacter = 0b1101010101010101u16;
 
 pub enum DisplayCharacter {
     Standard(StandardCharacter),
@@ -70,7 +79,6 @@ pub enum DisplayCharacter {
 
 impl DisplayCharacter {
     pub const fn new_from_ascii(character: u8) -> Self {
-        debug_assert!(character.is_ascii());
         match character.to_ascii_uppercase() {
             b'A' => return Self::Standard(A_PIXELS),
             b'B' => return Self::Standard(B_PIXELS),
@@ -109,13 +117,15 @@ impl DisplayCharacter {
             b'8' => return Self::Standard(EIGHT_PIXELS),
             b'9' => return Self::Standard(NINE_PIXELS),
             b'/' => return Self::Standard(SLASH_PIXELS),
+            b'.' => return Self::Small(DOT_PIXELS),
+            b',' => return Self::Small(COMMA_PIXELS),
             _ => return Self::Standard(UNKNOWN_PIXELS),
         };
     }
 }
 
-impl const Displayable for DisplayCharacter {
-    fn into_displaypixels(self) -> DisplayPixels {
+impl DisplayCharacter {
+    const fn into_displaypixels(self) -> DisplayPixels {
         let mut output: DisplayPixels = [0u128; 64];
         let mut i: usize = 0;
 
@@ -123,11 +133,7 @@ impl const Displayable for DisplayCharacter {
             Self::Standard(data) => {
                 const CHAR_PIXEL_MASK: u16 = 0x7000; // 0b01110000
                 while i < CHAR_LENGTH {
-                    let x = Into::<u128>::into(
-                        ((data & (CHAR_PIXEL_MASK >> (i * CHAR_WIDTH)))
-                            >> ((CHAR_LENGTH - (i + 1)) * CHAR_WIDTH))
-                            as u16,
-                    ); // what the fuck?
+                    let x: u128 = ((data & (CHAR_PIXEL_MASK >> (i * CHAR_WIDTH))) >> ((CHAR_LENGTH - (i + 1)) * CHAR_WIDTH) as u16) as u128;// what the fuck?
                     output[i] |= x;
                     i += 1;
                 }
@@ -136,11 +142,7 @@ impl const Displayable for DisplayCharacter {
             Self::Wide(data) => {
                 const CHAR_PIXEL_MASK: u32 = 0x7C000000;
                 while i < WIDECHAR_LENGTH {
-                    let x = Into::<u128>::into(
-                        ((data & (CHAR_PIXEL_MASK >> (i * WIDECHAR_WIDTH)))
-                            >> (((WIDECHAR_LENGTH - (i + 1)) * WIDECHAR_WIDTH) + 6))
-                            as u32,
-                    ); // what the fuck?
+                    let x = ((data & (CHAR_PIXEL_MASK >> (i * WIDECHAR_WIDTH))) >> (((WIDECHAR_LENGTH - (i + 1)) * WIDECHAR_WIDTH) + 6) as u32) as u128;
                     output[i] |= x;
                     i += 1;
                 }
@@ -149,11 +151,7 @@ impl const Displayable for DisplayCharacter {
             Self::Small(data) => {
                 const CHAR_PIXEL_MASK: u8 = 0xF;
                 while i < SMALLCHAR_LENGTH {
-                    let x = Into::<u128>::into(
-                        ((data & (CHAR_PIXEL_MASK >> (i * SMALLCHAR_WIDTH)))
-                            >> ((SMALLCHAR_LENGTH - (i + 1)) * SMALLCHAR_WIDTH))
-                            as u8,
-                    ); // what the fuck?
+                    let x: u128 = ((data & (CHAR_PIXEL_MASK >> (i * SMALLCHAR_WIDTH))) >> ((SMALLCHAR_LENGTH - (i + 1)) * SMALLCHAR_WIDTH) as u8) as u128;
                     output[i] |= x;
                     i += 1;
                 }
@@ -233,9 +231,19 @@ pub enum DataField {
     Gear(GearPosition),
 }
 
+
+#[macro_use]
+macro_rules! pixelate_character {
+    ($character:literal) => {
+        DisplayCharacter::into_displaypixels(DisplayCharacter::new_from_ascii($character))
+    };
+}
+
+const A_DISPLAY: DisplayPixels = pixelate_character!(b'a');
+const B_DISPLAY: DisplayPixels = pixelate_character!(b'b');
+
 pub fn main() {
-    const I: DisplayCharacter = DisplayCharacter::new_from_ascii(b'W');
-    for x in I.into_displaypixels() {
+    for x in A_DISPLAY {
         println!("{:#0128b}", x);
     }
 }
